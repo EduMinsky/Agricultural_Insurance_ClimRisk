4942b58bb5b608dfbb0ca9f3eda209db51bce896
EduMinsky <84152660+EduMinsky@users.noreply.github.com>
2024-01-24 14:48:47 -0300

    Update 14_Cosine_Similarity
    Exploring different distance metrics

--- 14_Cosine_Similarity.R
+++ 14_Cosine_Similarity.R
@@ -1,21 +1,30 @@
 library(tidyverse)
 library(conflicted)
-library(tidymodels)
-library(factoextra)
+library(lsa)
+set.seed(123)
 # Lendo o dataframe e criando k-means
 df_treino <- read_rds('./FinalData/DF_Treino.rds')
 df_treino <- na.omit(df_treino)
-df_treino <- df_treino%>%select(-c(ID,x,y,CRS))
+df_treino_var_only <- df_treino%>%select(-c(ID,x,y,CRS,Target))
 
-df_treino%>%select(-Target)
-res.km <- kmeans(scale(df_treino%>%select(-c(Target,ID,x,y,CRS))), centers = 3)
+res.km <- kmeans(scale(df_treino_var_only), centers = 3)
 cluster_filters <- res.km$cluster
 df_treino$Clusters <- cluster_filters
-# Vamos comparar os grupos
+# Vamos Separar os grupos
 cluster_1_Data <- df_treino%>%dplyr::filter(Clusters ==1)
 cluster_2_Data <- df_treino%>%dplyr::filter(Clusters ==2)
 cluster_3_Data <- df_treino%>%dplyr::filter(Clusters ==3)
 
-cluster_1_Data$anu_mean_temp_anomaly%>%mean
-cluster_2_Data$anu_mean_temp_anomaly%>%mean
-cluster_3_Data$anu_mean_temp_anomaly%>%mean
+#Criando nosso algoritmo de cosine
+cluster_1_Data%>%slice(1:3)%>%view
+
+# Vamos escolher manhattan ou cosseno?
+vec1 = c(-0.7873,-107.9503,51.4156,27.8733,13.8984,457.3835,335.0605,152.6665)
+vec2 = c(0.1137,-193.1271,60.1564,27.9348,43.3178,460.1475,198.0943,61.7119)
+vec3 = c(-0.6547,-119.6286,54.5428,25.2831,13.7925,396.365,365.187,89.6861)
+cosine(vec1,vec3)
+cosine(vec1,vec2)
+cosine(vec2,vec3)
+dist(rbind(vec1,vec2),method = "manhattan")
+
+scale(rbind(vec1,vec2))
