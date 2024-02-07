f3993ba6353182ce78f139f23cc06ac46c06bb81
EduMinsky <84152660+EduMinsky@users.noreply.github.com>
2024-02-06 23:53:52 -0300

    Update 12_Extracao_Var_Points.R

--- 12_Extracao_Var_Points.R
+++ 12_Extracao_Var_Points.R
@@ -8,24 +8,22 @@ library(tidyverse)
 #Lendo as variaveis
 variables <- terra::rast('./data_clim/Variables/Variables_present_ready.tif')
 
-#Lendo os shapefiles de treino:
-treino_event_0 <- read_sf('MiddleData/not_occurring_06_21_shp_unique.shp')
-treino_event_1 <- read_sf('MiddleData/occuring_06_21_shp_unique.shp')
-
-#Lendo o shapefile de teste:
-teste_event_0 <- read_sf('MiddleData/not_occurring_data_22_shp_unique.shp')
+#Lendo os shapefiles:
+data_0621_event_0 <- read_sf('MiddleData/not_occurring_06_21_shp_unique.shp')
+data_0621_event_1 <- read_sf('MiddleData/occuring_06_21_shp_unique.shp')
+data22_event_0 <- read_sf('MiddleData/not_occurring_data_22_shp_unique.shp')
 
 # Projetando nossos dados para South America Albers Equal Area
 # proj4: +proj=aea +lat_0=-32 +lon_0=-60 +lat_1=-5 +lat_2=-42 +x_0=0 +y_0=0 +ellps=aust_SA +units=m +no_defs +type=crs
 projecao <- '+proj=aea +lat_0=-32 +lon_0=-60 +lat_1=-5 +lat_2=-42 +x_0=0 +y_0=0 +ellps=aust_SA +units=m +no_defs +type=crs'
 variables_SA_Albers <- terra::project(x = variables,projecao, method = 'cubic')
-treino_event_0_SA_Albers <- st_transform(x = treino_event_0,
+treino_event_0_SA_Albers <- st_transform(x = data_0621_event_0,
                                         crs =projecao )
 
-treino_event_1_SA_Albers <- st_transform(x = treino_event_1,
+treino_event_1_SA_Albers <- st_transform(x = data_0621_event_1,
                                         crs =projecao )
 
-teste_event_0_SA_Albers <- st_transform(x = teste_event_0,
+teste_event_0_SA_Albers <- st_transform(x = data22_event_0,
                                         crs =projecao )
 
 
@@ -61,6 +59,6 @@ var_treino_event_1$Target = as.factor(1)
 var_teste_event_0$Target = as.factor(0)
 
 # Salvando como DataFrame essas tabelas:
-bind_rows(var_treino_event_0,var_treino_event_1)%>%write_rds('./FinalData/DF_Treino.rds')
-var_teste_event_0%>%write_rds('./FinalData/DF_Teste.rds')
+bind_rows(var_treino_event_0,var_treino_event_1,var_teste_event_0)%>%write_rds('./FinalData/DF_VAR.rds')
+getwd()
 
